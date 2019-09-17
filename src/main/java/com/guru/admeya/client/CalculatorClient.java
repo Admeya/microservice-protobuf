4ee7fbb54ce80d0013bf3a7b1271bb504cb977da
Irina Bykova <ibykova@x5.ru>
2019-09-17 07:51:56 +0300

    Обработка ошибок при вводе неверного числа

--- src/main/java/com/guru/admeya/client/CalculatorClient.java
+++ src/main/java/com/guru/admeya/client/CalculatorClient.java
@@ -8,6 +8,7 @@ import com.proto.greet.GreetServiceGrpc;
 import com.proto.greet.Greeting;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 
 public class CalculatorClient {
     public static void main(String[] args) {
@@ -19,14 +20,29 @@ public class CalculatorClient {
             .build();
         System.out.println("Creating stub");
 
-        CalcResultGrpc.CalcResultBlockingStub stub = CalcResultGrpc.newBlockingStub(channel);
-        Calculator.calcRequest request = Calculator.calcRequest.newBuilder().setFirstNumber(10)
-        .setSecondNumber(3).build();
-        Calculator.calcResponse response = stub.calculator(request);
-        System.out.println(response.getResult());
+//        CalcResultGrpc.CalcResultBlockingStub stub = CalcResultGrpc.newBlockingStub(channel);
+//        Calculator.calcRequest request = Calculator.calcRequest.newBuilder().setFirstNumber(10)
+//        .setSecondNumber(3).build();
+//        Calculator.calcResponse response = stub.calculator(request);
+//        System.out.println(response.getResult());
+
+        doErrorCall(channel);
 
         System.out.println("Shutting down");
         channel.shutdown();
 
+    }
+
+    private static void doErrorCall(ManagedChannel channel) {
+        CalcResultGrpc.CalcResultBlockingStub blockingStub = CalcResultGrpc.newBlockingStub(channel);
+        int number = -1;
+       try {
+           Calculator.SquareRootResponse result = blockingStub.squareRoot(Calculator.SquareRootRequest.newBuilder()
+               .setNumber(number)
+               .build());
+       } catch (StatusRuntimeException e) {
+           System.out.println("Got an exception for square root! ");
+           e.printStackTrace();
+       }
     }
 }
--- src/main/java/com/guru/admeya/server/controllers/CalculatorServerImpl.java
+++ src/main/java/com/guru/admeya/server/controllers/CalculatorServerImpl.java
@@ -6,6 +6,7 @@ import com.proto.greet.GreetRequest;
 import com.proto.greet.GreetResponse;
 import com.proto.greet.GreetServiceGrpc;
 import com.proto.greet.Greeting;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class CalculatorServerImpl extends CalcResultGrpc.CalcResultImplBase {
@@ -13,8 +14,32 @@ public class CalculatorServerImpl extends CalcResultGrpc.CalcResultImplBase {
     @Override
     public void calculator(Calculator.calcRequest request,
         StreamObserver<Calculator.calcResponse> responseObserver) {
-        Calculator.calcResponse response = Calculator.calcResponse.newBuilder().setResult(request.getFirstNumber()+request.getSecondNumber()).build();
+        Calculator.calcResponse response = Calculator.calcResponse.newBuilder()
+            .setResult(request.getFirstNumber() + request.getSecondNumber()).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void squareRoot(Calculator.SquareRootRequest request,
+        StreamObserver<Calculator.SquareRootResponse> responseObserver) {
+        int number = request.getNumber();
+
+        if (number >= 0) {
+            double numberRoot = Math.sqrt(number);
+            responseObserver.onNext(
+                Calculator.SquareRootResponse.newBuilder()
+                    .setNumberRoot(numberRoot)
+                    .build()
+            );
+        } else {
+            // we construct the exception
+            responseObserver.onError(
+                Status.INVALID_ARGUMENT
+                    .withDescription("The number being sent is not positive")
+                    .augmentDescription("Number sent: " + number)
+                    .asRuntimeException()
+            );
+        }
     }
 }
