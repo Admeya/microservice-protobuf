eb85ab0f7fe2a0f7abe0a1994a5e44dff0b17143
Admeya <lugovova@live.ru>
2019-11-09 11:21:23 +0300

    Add update Blog document

--- src/main/java/com/guru/admeya/blog/client/BlogClient.java
+++ src/main/java/com/guru/admeya/blog/client/BlogClient.java
@@ -6,6 +6,8 @@ import com.proto.blog.CreateBlogRequest;
 import com.proto.blog.CreateBlogResponse;
 import com.proto.blog.ReadBlogRequest;
 import com.proto.blog.ReadBlogResponse;
+import com.proto.blog.UpdateBlogRequest;
+import com.proto.blog.UpdateBlogResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -16,6 +18,18 @@ public class BlogClient {
         BlogClient main = new BlogClient();
         main.run();
         BlogServiceGrpc.BlogServiceBlockingStub blogClient = BlogServiceGrpc.newBlockingStub(channel);
+
+        CreateBlogResponse createBlogResponse = createDocument(blogClient);
+        String blogId = readDocument(blogClient, createBlogResponse);
+        updateDocument(blogClient, blogId);
+
+        // created a greet service client (blocking - synchronous
+        System.out.println("Shutting down");
+        channel.shutdown();
+
+    }
+
+    private static CreateBlogResponse createDocument(BlogServiceGrpc.BlogServiceBlockingStub blogClient) {
         Blog blog = Blog.newBuilder()
             .setAuthorId("Irina")
             .setTitle("New blog!")
@@ -30,7 +44,11 @@ public class BlogClient {
 
         System.out.println("Receive create blog response");
         System.out.println(createBlogResponse.toString());
+        return createBlogResponse;
+    }
 
+    private static String readDocument(BlogServiceGrpc.BlogServiceBlockingStub blogClient,
+        CreateBlogResponse createBlogResponse) {
         String blogId = createBlogResponse.getBlog().getId();
 
         System.out.println("I found just created message!");
@@ -41,23 +59,34 @@ public class BlogClient {
         System.out.println(response.toString());
 
         // example of Not found
-//        System.out.println("I do not found message");
-//        ReadBlogResponse notFound = blogClient.readBlog(ReadBlogRequest.newBuilder()
-//            .setBlogId("fakeId")
-//            .build());
+        //        System.out.println("I do not found message");
+        //        ReadBlogResponse notFound = blogClient.readBlog(ReadBlogRequest.newBuilder()
+        //            .setBlogId("fakeId")
+        //            .build());
+        return blogId;
+    }
 
-        // created a greet service client (blocking - synchronous
-        System.out.println("Shutting down");
-        channel.shutdown();
+    private static void updateDocument(BlogServiceGrpc.BlogServiceBlockingStub blogClient, String blogId) {
+        Blog newBlog = Blog.newBuilder()
+            .setAuthorId("Changed Author")
+            .setTitle("New blog (Updated)!")
+            .setContent("(Update blog message)")
+            .setId(blogId)
+            .build();
 
+        UpdateBlogResponse responseUpdate = blogClient.updateBlog(UpdateBlogRequest.newBuilder()
+            .setBlog(newBlog).build());
+
+        System.out.println("Updated blog ");
+        System.out.println(responseUpdate.toString());
     }
 
     protected void run() {
         System.out.println("hello i'm a gRPC client");
-                channel = ManagedChannelBuilder
-                    .forAddress("localhost", 50051)
-                    .usePlaintext()
-                    .build();
+        channel = ManagedChannelBuilder
+            .forAddress("localhost", 50051)
+            .usePlaintext()
+            .build();
         System.out.println("Creating stub");
     }
 }
--- src/main/java/com/guru/admeya/blog/server/BlogServerImpl.java
+++ src/main/java/com/guru/admeya/blog/server/BlogServerImpl.java
@@ -10,6 +10,8 @@ import com.proto.blog.CreateBlogRequest;
 import com.proto.blog.CreateBlogResponse;
 import com.proto.blog.ReadBlogRequest;
 import com.proto.blog.ReadBlogResponse;
+import com.proto.blog.UpdateBlogRequest;
+import com.proto.blog.UpdateBlogResponse;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.bson.Document;
@@ -62,11 +64,9 @@ public class BlogServerImpl extends BlogServiceGrpc.BlogServiceImplBase {
         System.out.println("Received Read Blog request");
 
         String blogId = request.getBlogId();
-        System.out.println("Searching for a blog");
         Document result = null;
         try {
-            result = collection.find(eq("_id", new ObjectId(blogId)))
-                .first();
+            result = getDocument(blogId);
         } catch (Exception e) {
             responseObserver.onError(
                 Status.NOT_FOUND
@@ -76,7 +76,7 @@ public class BlogServerImpl extends BlogServiceGrpc.BlogServiceImplBase {
         }
 
         if (result == null) {
-            // we don't hsve a match
+            // we don't have a match
             System.out.println("Blog not found");
             responseObserver.onError(
                 Status.NOT_FOUND
@@ -85,13 +85,69 @@ public class BlogServerImpl extends BlogServiceGrpc.BlogServiceImplBase {
             );
         } else {
             System.out.println("Blog found, sending response");
-            Blog blog = Blog.newBuilder().setAuthorId(result.getString("author_id"))
-                .setTitle(result.getString("title"))
-                .setContent(result.getString("content"))
-                .setId(blogId)
-                .build();
+            Blog blog = documentToBlog(blogId, result);
             responseObserver.onNext(
                 ReadBlogResponse.newBuilder().setBlog(blog).build()
+            );
+            responseObserver.onCompleted();
+        }
+    }
+
+    private Blog documentToBlog(String blogId, Document result) {
+        return Blog.newBuilder().setAuthorId(result.getString("author_id"))
+                    .setTitle(result.getString("title"))
+                    .setContent(result.getString("content"))
+                    .setId(blogId)
+                    .build();
+    }
+
+    private Document getDocument(String blogId) {
+        System.out.println("Searching for a blog");
+        return collection.find(eq("_id", new ObjectId(blogId)))
+            .first();
+    }
+
+    @Override
+    public void updateBlog(UpdateBlogRequest request, StreamObserver<UpdateBlogResponse> responseObserver) {
+        System.out.println("Received Update Blog request");
+
+        Blog blog = request.getBlog();
+        String blogId = blog.getId();
+        Document result = null;
+        try {
+            result = getDocument(blogId);
+            System.out.println("so we can update it");
+        } catch (Exception e) {
+            responseObserver.onError(
+                Status.NOT_FOUND
+                    .augmentDescription(e.getLocalizedMessage())
+                    .asRuntimeException()
+            );
+        }
+
+        if (result == null) {
+            // we don't have a match
+            System.out.println("Blog not found");
+            responseObserver.onError(
+                Status.NOT_FOUND
+                    .withDescription("The blog with the corresponding id was not found")
+                    .asRuntimeException()
+            );
+        } else {
+            System.out.println("Blog found, update document");
+
+            Document replacementDoc = new Document("author_id", blog.getAuthorId())
+                .append("content", blog.getContent())
+                .append("title", blog.getTitle());
+
+            System.out.println("Replacing blog in database...");
+
+            collection.replaceOne(eq("_id", result.getObjectId("_id")), replacementDoc);
+            Blog resultBlog = documentToBlog(blogId, replacementDoc);
+
+            System.out.println("Replaced! Send the response!");
+            responseObserver.onNext(
+                UpdateBlogResponse.newBuilder().setBlog(resultBlog).build()
             );
             responseObserver.onCompleted();
         }
